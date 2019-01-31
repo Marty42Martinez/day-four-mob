2d7238752b875c5bf4cbabe712043f06b5c45c75
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-31 14:06:27 -0800

    added bug head, segments, css styling, makeSegment function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const fruitButtons = document.getElementById('fruit-buttons');
+const bugBody = document.getElementById('bug-body');
 const fruitArray = ['apple', 'banana', 'pear', 'blueberry', 'orange', 'grape'];
+
+function makeSegment(fruit) {
+    const segment = document.createElement('span');
+    segment.classList.add('segment', fruit);
+    bugBody.appendChild(segment);
+}
 
 for(let i = 0; i < fruitArray.length; i++) {
     const fruit = fruitArray[i];
-    console.log(fruit);
     const fruitButton = document.createElement('button');
     fruitButton.value = fruit;
     fruitButton.textContent = fruit;
     fruitButton.classList.add('fruit-button', fruit);
-    console.log(fruitButton);
     fruitButtons.appendChild(fruitButton);
+
+    fruitButton.addEventListener('click', function(event) {
+        makeSegment(fruit);
+    });
 }
 
-// console.log('fruit button', fruitButtons);
